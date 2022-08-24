408787ae519af5ea48005c90eeee26201d83e9e1
5678 <1255520995@qq.com>
2022-08-24 10:52:35 +0800

    refactor: 项目架构搭建完成

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h1>sell manage  </h1>
+    <i class="iconfont icon-user"></i>
+    <el-button>按钮</el-button>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui';
+
+// 引入默认样式
+import '@/assets/css/property.css';
+import '@/assets/css/reset.css';
+// 引入字体图标
+import '@/assets/fonts/iconfont.css';
+// 引入ElementUI
+import 'element-ui/lib/theme-chalk/index.css';
+
 
 Vue.config.productionTip = false
 
+// 调用ElementUI
+Vue.use(ElementUI);
+
+
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
